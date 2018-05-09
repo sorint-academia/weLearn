f429ca034aa85b46f6bf6f3495404e63e1ce4767
amreo <amreo@live.it>
2018-05-09 13:49:12 +0200

    Completed CourseService

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Course } from '../entity/course';
+import { Unit } from '../entity/unit';
+import { Widget } from '../entity/widget';
 import { of } from 'rxjs/observable/of';
 import { LoggerService } from './logger.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -19,5 +21,16 @@ export class CoursesService {
     this.loggerService.log("requesting the course detail: " + courseID);
     return this.http.get<Course>(environment.backendBaseUrl+courseID);
   }
-
+  getCourseUnits(courseID: String): Observable<Unit[]> {
+    this.loggerService.log("requesting the course units: " + courseID);
+    return this.http.get<Unit[]>(environment.backendBaseUrl+courseID+"/units");
+  }
+  getUnit(unitID : String): Observable<Unit> {
+    this.loggerService.log("requesting the unit: " + unitID);
+    return this.http.get<Unit>(environment.backendBaseUrl+unitID);
+  }
+  getUnitWidgets(unitID : String): Observable<Widget[]> {
+    this.loggerService.log("requesting the unit widgets: " + unitID);
+    return this.http.get<Widget[]>(environment.backendBaseUrl+unitID+"/widgets");
+  }
 }
